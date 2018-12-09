ea26854e7f50d3c7afe4ca1dbec4d6a476a41648
Sebek <31373015+sebekos@users.noreply.github.com>
2018-12-09 10:19:10 -0600

    Update auth.js

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -47,9 +47,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCMs-qKzcpmogW9AFbypz_-f3BS5nFkoj4';
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=';
         if(!isSignup){
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCMs-qKzcpmogW9AFbypz_-f3BS5nFkoj4';
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=';
         }
         axios.post(url, authData)
         .then(response => {
